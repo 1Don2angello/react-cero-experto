99ee35c7bf73228663a6159c0c9516bc6b5f238d
Perez <106407139+1Don2angello@users.noreply.github.com>
2024-10-23 10:41:27 -0500

    update

---
+++ To-do-api/To-do-api/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApi.Models;
+
+namespace To_do_api.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            { 
+            /*MIGRACION DE LA BD*/
+       
+        }
+        public DbSet<TaskItem> Tasks { get; set; }
+
+
+    }
+}
---
+++ To-do-api/To-do-api/Models/ToDoItem.cs
@@ -0,0 +1,22 @@
+﻿namespace ToDoApi.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Materia { get; set; }
+        public DateTime FechaRecibida { get; set; }
+        public DateTime FechaEntrega { get; set; }
+        public int DiasRestantes
+        {
+            get
+            {
+                return (FechaEntrega - DateTime.Today).Days;
+            }
+        }
+        public bool Urgente { get; set; }
+        public string Prioridad { get; set; }
+        public string? Documentos { get; set; }
+        public string Tipo { get; set; }
+    }
+}
