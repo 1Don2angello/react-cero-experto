4c81448723bbc3a03999c3a33322a95e573e588a
Perez <106407139+1Don2angello@users.noreply.github.com>
2024-11-27 17:28:41 -0500

    comit 1

--- mini-project/utils/logger.ts
+++
@@ -1,7 +0,0 @@
-export const logInfo = (message: string, data?: object) => {
-  console.log(`[INFO] ${message}`, data || {});
-};
-
-export const logError = (message: string, error?: Error) => {
-  console.error(`[ERROR] ${message}`, error || {});
-};
