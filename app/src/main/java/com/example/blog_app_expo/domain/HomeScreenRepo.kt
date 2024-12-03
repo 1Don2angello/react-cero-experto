3e1766c21bd296cbf0454a43069ce909491ea498
Perez <106407139+1Don2angello@users.noreply.github.com>
2024-12-03 12:43:58 -0500

    update

--- app/src/main/java/com/example/blog_app_expo/domain/HomeScreenRepo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.blog_app_expo.domain
-
-
-import com.example.blog_app_expo.data.model.Post
-
-interface HomeScreenRepo {
-    suspend fun getLatestPosts(): List<Post>
-    suspend fun addPost(post: Post)
-}
--- app/src/main/java/com/example/blog_app_expo/domain/HomeScreenRepoImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.example.blog_app_expo.domain
-
-import com.example.blog_app_expo.data.model.Post
-import com.example.blog_app_expo.data.HomeScreenDataSource
-
-class HomeScreenRepoImpl(private val dataSource: HomeScreenDataSource) : HomeScreenRepo {
-    override suspend fun getLatestPosts(): List<Post> = dataSource.getLatestPosts()
-    override suspend fun addPost(post: Post) = dataSource.addPost(post)
-}
